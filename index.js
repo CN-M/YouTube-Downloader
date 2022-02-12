3ad9656a9c1efa9e57b0b959aa89f43eabe5e0ce
Ntsako Mbhalati <cn.mbhalati@gmail.com>
2022-02-12 23:24:17 +0200

    Made a function to call download

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const fs = require('fs');
 const ytdl = require('ytdl-core');
 
-ytdl('https://www.youtube.com/watch?v=wvwUM_cjpj0')
+const link = 'https://www.youtube.com/watch?v=vrzSFgjBM80';
+
+function download(link) {
+    console.log('Download Start')
+    ytdl(link)
     .pipe(fs.createWriteStream('video.mp4'));
+    console.log('Download Complete')
+}
+
+download(link);
