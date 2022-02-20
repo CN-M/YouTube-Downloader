54d2e5880e4e9f8bf40d3c92c6d8002c09f10303
Ntsako Mbhalati <cn.mbhalati@gmail.com>
2022-02-20 18:04:13 +0200

    Just a few tests

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const ytdl = require('ytdl-core');
 
-const link = 'https://www.youtube.com/watch?v=vrzSFgjBM80';
+const link = 'https://www.youtube.com/watch?v=vqp9yoAP97Y';
 
 function download(link) {
     console.log('Download Start')
     ytdl(link)
-    .pipe(fs.createWriteStream('video.mp4'));
-    console.log('Download Complete')
+    .pipe(fs.createWriteStream('Baby5.mp4'));
 }
 
 download(link);
+console.log('Download Complete')
